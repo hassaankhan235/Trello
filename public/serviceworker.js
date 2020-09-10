cda2b410ca18e5c95a3424c9afb66c83e2e76f2c
hassaankhan235 <hassaankhan235@gmail.com>
2020-09-10 13:25:35 +0300

    chk

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -9,8 +9,8 @@ this.addEventListener('install', (event) => {
           'index.html',
           '/',
           './static/js/2.c02aa915.chunk.js',
-          './static/main.css',
-          './static/main.js',
+          './static/js/main.css',
+          './static/js/main.js',
           './static/js/2.c02aa915.chunk.js.map', 
           './manifest.json'
         ])
