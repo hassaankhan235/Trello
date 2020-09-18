292888500434493b768ff16f0e7c9b84ea9ec2d7
hassaankhan235 <hassaankhan235@gmail.com>
2020-09-18 14:54:43 +0300

    copy

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -18,26 +18,28 @@ this.addEventListener('install', (event) => {
 })
 
 
-self.addEventListener('fetch', event => {
-  event.respodWith(
+self.addEventListener('fetch', function(event)  {
+  event.respondWith(
     caches.match(event.request)
-    .then(function(response){
-      if(response){
-        return response
+    .then(function(response) {
+      if (response) {
+        return response;
       }
-      var requestToCache = event.request.clone()
+      var requestToCache = event.request.clone();
       return fetch(requestToCache).then(
-        function(response){
-          if(!response || response.status !== 200 ){
-            return response
+        function(response) {
+          if(!response || response.status !== 200) {
+            return response;
           }
-          var responseToCache = response.clone()
-          caches.open(cacheName).then(function(cache){
-            cache.put(requestToCache, responseToCache)
-          })
+          var responseToCache = response.clone();
+          caches.open(cacheName)
+          .then(function(cache) { 
+            cache.put(requestToCache, responseToCache);
+          });
           return response;
         }
-      )
-    })
-  )
-})
+        );
+      })
+      );
+      });
+      
